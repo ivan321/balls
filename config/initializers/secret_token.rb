107a82b87f09bab83c8c3845c83e3729353af44f
ivan321 <ivanbutler@gmail.com>
2012-11-18 21:34:49 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balls::Application.config.secret_token = 'bb5c5d7d11e6865c230c42f049d06d30c96252359b07fb90c48f73f214e3c6393814af3fb8f3dd2847e45b17038fa75eeb2ba1f6062af52e991462c81264e04a'
